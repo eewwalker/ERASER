3161a546e253fc71e6795a1af48f93b912b07e6d
Jazz Cheema <jaskamalc@gmail.com>
2024-04-16 11:45:01 -0700

    first commit

---
+++ pixly-frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import './App.css';
+
+
+/** Component for entire page.
+ *
+ * Props: none
+ * State: none
+ *
+*/
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <main>
+      Pixly.
+      </main>
+    </div>
+  );
+};
+
+export default App;
