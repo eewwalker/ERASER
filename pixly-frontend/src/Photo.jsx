52779d64d7a6e79599baa450448c73fcfc7375e3
Jazz Cheema <jaskamalc@gmail.com>
2024-04-17 18:52:57 -0700

    push

--- pixly-frontend/src/Photo.jsx
+++ pixly-frontend/src/Photo.jsx
@@ -6,16 +6,18 @@ import React from "react";
  *
  */
 
-const S3_URL = "https://pixlyappp.s3.us-west-1.amazonaws.com";
+const S3_JAZZ = "https://pixly-app-bucket.s3.us-west-1.amazonaws.com";
+const S3_EMILY = "https://pixlyappp.s3.us-west-1.amazonaws.com";
+
 
 
 // add dynamic image parameter below.
 
 const Photo = ({ photo }) => {
-  console.log('photo', photo);
+  console.log("photo", photo);
   return (
     <div className="Photo">
-      <img src={`https://pixlyappp.s3.us-west-1.amazonaws.com/cat.jpeg`} alt="photo" />
+      <img src={`${S3_JAZZ}/${photo}`} alt="photo" />
     </div>
   );
 };
