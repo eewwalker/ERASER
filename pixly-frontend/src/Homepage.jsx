23d0928c19e00ea67e401be3fed2bb3ebd9eabd0
Jazz Cheema <jaskamalc@gmail.com>
2024-04-19 01:13:45 -0700

    updated worldmap on splash

--- pixly-frontend/src/Homepage.jsx
+++ pixly-frontend/src/Homepage.jsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { PageHeader, Grommet, Page, PageContent, Paragraph, WorldMap } from "grommet";
+import {
+  PageHeader,
+  Grommet,
+  Page,
+  PageContent,
+  Paragraph,
+  WorldMap,
+} from "grommet";
 
 /** Component for Homepage.
  *
@@ -9,26 +16,45 @@ import { PageHeader, Grommet, Page, PageContent, Paragraph, WorldMap } from "gro
  * App -> RoutesList -> Homepage
  */
 const Homepage = () => {
-    return (
-      <Grommet full>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <Page kind="narrow">
-          <PageContent>
-            <Paragraph alignSelf="center" style={{ fontSize: "60px" }}>ERASER</Paragraph>
-            <Paragraph alignSelf="center">Upload to erase your image.</Paragraph>
-            <Paragraph alignSelf="center" style={{ fontSize: "14px" }}>
-              1,000 users a day are waiting to erase your image...
-            </Paragraph>
-              <WorldMap/>
-          </PageContent>
-        </Page>
-      </Grommet>
-    );
-  };
+  return (
+    <Grommet full>
+      <br />
+      <br />
+      <br />
+      <br />
+      <Page kind="narrow">
+        <PageContent>
+          <Paragraph alignSelf="center" style={{ fontSize: "60px" }}>
+            ERASER
+          </Paragraph>
+          <Paragraph alignSelf="center">Upload to erase your image.</Paragraph>
+          <Paragraph alignSelf="center" style={{ fontSize: "14px" }}>
+            1,000 users a day are waiting to erase your image...
+          </Paragraph>
+          <WorldMap
+            color="graph-0"
+            continents={[
+              {
+                name: "Africa",
+                color: "graph-1",
+                onClick: (name) => {},
+              },
+            ]}
+            onSelectPlace={(lat, lon) => {}}
+            places={[
+              {
+                name: "Sydney",
+                location: [-33.8830555556, 151.216666667],
+                color: "graph-2",
+                onClick: (name) => {},
+              },
+            ]}
+            selectColor="brand"
+          />
+        </PageContent>
+      </Page>
+    </Grommet>
+  );
+};
 
 export default Homepage;
